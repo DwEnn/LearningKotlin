fad4db69a7a093d54c014ef99abfdcd8a010c83c
김선우 <prgohn@gmail.com>
2018-08-04 17:56:49 +0900

    Updating Java to Kotlin

---
+++ src/K1000.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+
+//    var A = sc.nextInt()
+//    var B = sc.nextInt()
+
+//    print(A+B)
+
+    print(sc.nextInt() + sc.nextInt())
+}
