937fb28f9abb50ed0b57627e9b31b1d3145469a2
김선우 <prgohn@gmail.com>
2018-08-09 16:54:12 +0900

    Upload BSIS Code Festival Q)B

---
+++ src/BSISCodeFestival/BSISB.kt
@@ -0,0 +1,14 @@
+package BSISCodeFestival
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val N = sc.nextDouble()
+    val M = sc.nextDouble()
+
+    if (N == M)
+        println(1)
+    else
+        println(0)
+}
